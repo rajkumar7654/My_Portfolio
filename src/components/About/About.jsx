355cc629bf9cfae2459bd2d41f150527f2d02b68
rajkumar7654 <rk578568@gmail.com>
2026-01-10 19:49:49 +0530

    update new resume link

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -45,7 +45,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1gQWlD7rCFrIuTLedZVsAqQNLKKz439i0/view?usp=drivesdk"
+            href="https://drive.google.com/file/d/1tbvFWVPCTVpFHgYftTluzv7SWtpZZ1tz/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
